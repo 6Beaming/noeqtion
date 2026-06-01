2929a81d12f35823da28aaaf2fd993fa3205f5e7
Eric Liu <beaming6666@gmail.com>
2026-06-01 10:04:32 -0400

    Updated URL validation to allow any URL from Notion domains.

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
 const api = typeof browser !== "undefined" ? browser : chrome;
 
 api.action.onClicked.addListener((tab) => {
-  if (!tab?.id || !tab.url?.startsWith("https://www.notion.so/")) {
+  if (!tab?.id || !tab.url) {
+    return;
+  }
+
+  // Matches https://notion.so, https://www.notion.so, https://app.notion.com, etc.
+  const isValidNotionUrl = /^https:\/\/(.*\.)?notion\.(so|com)(\/|$)/.test(tab.url);
+
+  if (!isValidNotionUrl) {
     return;
   }
 
